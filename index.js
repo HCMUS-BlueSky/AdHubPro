4b21260524a13c7e1039eed2ea3427d9d8b9710c
Minh Khoi <nminhkhoi0818@gmail.com>
2023-10-17 14:22:59 +0700

    Edit card info marker

--- index.js
+++ index.js
@@ -32,7 +32,18 @@ async function initMap() {
     // markers can only be keyboard focusable when they have click listeners
     // open info window when marker is clicked
     marker.addListener("click", () => {
-      infoWindow.setContent(position.lat + ", " + position.lng);
+      const cardInfo = document.createElement("div");
+      const headerInfo = document.createElement("h3");
+      headerInfo.textContent = "Biểu tình Hang Pác Pó";
+      const positionInfo = document.createElement("p");
+      positionInfo.textContent =
+        "Đất công/Công viên/Hành lang an toàn giao thông";
+      const zoningStatus = document.createElement("p");
+      zoningStatus.textContent = "Đã quy hoạch";
+      cardInfo.appendChild(headerInfo);
+      cardInfo.appendChild(positionInfo);
+      cardInfo.appendChild(zoningStatus);
+      infoWindow.setContent(cardInfo);
       infoWindow.open(map, marker);
     });
     return marker;
@@ -43,29 +54,12 @@ async function initMap() {
 }
 
 const locations = [
-  { lat: -31.56391, lng: 147.154312 },
-  { lat: -33.718234, lng: 150.363181 },
-  { lat: -33.727111, lng: 150.371124 },
-  { lat: -33.848588, lng: 151.209834 },
-  { lat: -33.851702, lng: 151.216968 },
-  { lat: -34.671264, lng: 150.863657 },
-  { lat: -35.304724, lng: 148.662905 },
-  { lat: -36.817685, lng: 175.699196 },
-  { lat: -36.828611, lng: 175.790222 },
-  { lat: -37.75, lng: 145.116667 },
-  { lat: -37.759859, lng: 145.128708 },
-  { lat: -37.765015, lng: 145.133858 },
-  { lat: -37.770104, lng: 145.143299 },
-  { lat: -37.7737, lng: 145.145187 },
-  { lat: -37.774785, lng: 145.137978 },
-  { lat: -37.819616, lng: 144.968119 },
-  { lat: -38.330766, lng: 144.695692 },
-  { lat: -39.927193, lng: 175.053218 },
-  { lat: -41.330162, lng: 174.865694 },
-  { lat: -42.734358, lng: 147.439506 },
-  { lat: -42.734358, lng: 147.501315 },
-  { lat: -42.735258, lng: 147.438 },
-  { lat: -43.999792, lng: 170.463352 },
+  { lat: 10.762993690850745, lng: 106.68247166663183 },
+  { lat: 10.763747306708495, lng: 106.68255749732144 },
+  { lat: 10.758647557139085, lng: 106.6817497288846 },
+  { lat: 10.76478718852813, lng: 106.67819311972303 },
+  { lat: 10.76837842784627, lng: 106.67832310249808 },
+  { lat: 10.773210602249506, lng: 106.67757225283157 },
 ];
 
 initMap();
