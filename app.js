fd95ddd0b6a30512f2dca8cfeefa862e55fa4014
Minh Khoi <nminhkhoi0818@gmail.com>
2023-10-19 10:16:27 +0700

    Added ejs-mate template

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const ejsMate = require("ejs-mate");
 const homeRoutes = require("./routes/home");
 const officer = require("./routes/officer");
 
+app.engine("ejs", ejsMate);
+
+app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 
 app.use("/", homeRoutes);
