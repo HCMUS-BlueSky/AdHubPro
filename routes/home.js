f5badc1a628d8be319179329fb80cca11d72a50f
Minh Khoi <nminhkhoi0818@gmail.com>
2023-10-23 22:54:26 +0700

    Changed some style for side bar

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  // const locations = await Location.find({}).exec();
+  // locations = json(locations);
+  // console.log(locations);
   res.render("index");
 });
 
