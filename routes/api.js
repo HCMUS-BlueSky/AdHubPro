0edc85245b61945b100eb7c40fc270e6301d7c55
PSDat123 <sieudat123@gmail.com>
2023-10-24 20:54:40 +0700

    Add file upload & ads api

--- routes/api.js
+++ routes/api.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const locationAPI = require('./api/location');
+const adsAPI = require('./api/ads');
 
 router.use('/location', locationAPI);
+router.use('/ads', adsAPI);
 
 module.exports = router;
--- routes/api/location.js
+++ routes/api/location.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const Location = require('../../models/Location');
+const Ads = require('../../models/Ads');
+const upload = require('../../middleware/multer');
+const uploadFile = require('../../utils/fileUpload');
 const router = express.Router();
 
 
@@ -12,10 +15,49 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.post('/', async (req, res) => {
+// function makeid(length) {
+//   let result = '';
+//   const characters =
+//     'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+//   const charactersLength = characters.length;
+//   let counter = 0;
+//   while (counter < length) {
+//     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+//     counter += 1;
+//   }
+//   return result;
+// }
+
+// function generateRandomDate(from, to) {
+//   return new Date(
+//     from.getTime() + Math.random() * (to.getTime() - from.getTime())
+//   );
+// }
+
+// router.post('/', upload.array("images", 5) ,async (req, res) => {
 //   try {
-//     const location = new Location(req.body);
-//     await location.save();
+//     // console.log(req.files);
+//     const urls = []
+//     for(let file of req.files) {
+//       const url = await uploadFile(`adhubpro/ads`, file);
+//       urls.push(url);
+//     }
+//     console.log(urls);
+//     const locations = await Location.find({}).exec();
+//     for(let location of locations) {
+//       const ads = new Ads({
+//         location: location.id,
+//         type: makeid(20),
+//         size: makeid(8),
+//         images: urls,
+//         expiration: generateRandomDate(
+//           new Date(2023, 11, 1),
+//           new Date(2024, 11, 1)
+//         )
+//       });
+//       console.log(ads)
+//       await ads.save();
+//     }
 //     return res.send("SUCCESS")
 //   } catch (err) {
 //     return res.status(500).send(err.message);
