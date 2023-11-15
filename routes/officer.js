3ea547f80888e303cb2b3002bd93fd5d69a28e16
PSDat123 <sieudat123@gmail.com>
2023-11-15 08:45:57 +0700

    feat(api): add protected routing

--- routes/officer.js
+++ routes/officer.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const pathFor = require("../middleware/pathFor");
 const router = express.Router();
 
-router.get("/ward", (req, res) => {
-  res.render("officer/officerWard");
+router.get('/', (req, res) => {
+  // login
+  return res.redirect(302, '/');
 });
 
-router.get("/district", (req, res) => {
-  res.render("officer/officerDistrict");
+router.get('/ward', pathFor('ward_officer'), (req, res) => {
+  res.render('officer/officerWard');
 });
 
-router.get("/headquarter", (req, res) => {
+router.get('/district', pathFor('district_officer'), (req, res) => {
+  res.render('officer/officerDistrict');
+});
+
+router.get("/department", pathFor('department_officer'), (req, res) => {
   res.render("officer/officerHeadquarter");
 });
 
