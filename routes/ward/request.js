76887af9869f40487aee1de0c36a982c61790e6f
KitoKotowa <chaukiet220803@gmail.com>
2023-11-20 18:46:23 +0700

    request: add create new

--- routes/ward/request.js
+++ routes/ward/request.js
@@ -6,6 +6,6 @@ router.get("/", requestController.view);
 
 router.get("/view/:id", requestController.getDetail);
 
-router.get("/create/:id", requestController.createNew);
+router.get("/create", requestController.createNew);
 
 module.exports = router;
