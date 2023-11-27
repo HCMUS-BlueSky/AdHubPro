087a0844bc4cdc95d66361017f229deb15f86302
PSDat123 <sieudat123@gmail.com>
2023-11-27 18:07:10 +0700

    chore: edit firebase config

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,11 @@
 const admin = require('firebase-admin');
 
 const firebaseConfig = {
-  credential: admin.credential.cert('firebaseServiceCred.json'),
+  credential: admin.credential.cert(
+    process.env.NODE_ENV == 'production'
+      ? '/etc/secrets/firebaseServiceCred.json'
+      : 'firebaseServiceCred.json'
+  ),
   storageBucket: 'adhubpro-d78fc.appspot.com'
 };
 
