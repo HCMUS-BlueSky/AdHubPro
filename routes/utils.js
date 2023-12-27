96a4cbd5cf320b422fce25454b8bf23e77814b9b
Minh Khoi <nminhkhoi0818@gmail.com>
2023-12-27 22:50:26 +0700

    feat: add filter request for department officer

--- routes/utils.js
+++ routes/utils.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.get("/districts", utilsController.getDistricts);
 router.get("/districts/:id/wards", utilsController.getWards);
+router.get("/reports/count", utilsController.getReports);
+router.get("/reports/monthlyCount", utilsController.getReportsByMonth);
 
 module.exports = router;
