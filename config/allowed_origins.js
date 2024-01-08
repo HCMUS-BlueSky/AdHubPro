98126eb372d91c6acff3b0f6da997668c512ae5c
Khoi Nguyen <nminhkhoi0818@gmail.com>
2024-01-08 09:36:05 +0700

    add cors for debug map

--- config/allowed_origins.js
+++ config/allowed_origins.js
@@ -1,8 +1,9 @@
 const origins = [
-  'http://127.0.0.1:4000',
-  'http://localhost:4000',
-  'https://adhubpro.onrender.com',
-  'https://cms-adhubpro.onrender.com'
+  "http://127.0.0.1:4000",
+  "http://localhost:4000",
+  "https://adhubpro.onrender.com",
+  "https://cms-adhubpro.onrender.com",
+  "http://127.0.0.1:5500/",
 ];
 
 module.exports = origins;
