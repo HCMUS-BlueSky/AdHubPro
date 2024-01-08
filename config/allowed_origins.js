6ba7391b6564fbdb13271bf0f339f8dc8156dad0
Khoi Nguyen <nminhkhoi0818@gmail.com>
2024-01-08 09:46:08 +0700

    hot fix

--- config/allowed_origins.js
+++ config/allowed_origins.js
@@ -3,7 +3,7 @@ const origins = [
   "http://localhost:4000",
   "https://adhubpro.onrender.com",
   "https://cms-adhubpro.onrender.com",
-  "http://127.0.0.1:5500/",
+  "http://127.0.0.1:5500",
 ];
 
 module.exports = origins;
